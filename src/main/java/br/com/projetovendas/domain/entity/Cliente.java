191b3b576ea2832b1afae7259409962757b0e8c3
Carlos Henrique <c.heeenrique@gmail.com>
2022-04-10 16:18:30 -0300

    Criação de cadastro do cliente com JdbcTemplate

--- src/main/java/br/com/projetovendas/domain/entity/Cliente.java
+++ src/main/java/br/com/projetovendas/domain/entity/Cliente.java
@@ -1,2 +1,23 @@
-package br.com.projetovendas.domain;public class entity {
+package br.com.projetovendas.domain.entity;
+
+public class Cliente {
+
+    private Integer id;
+    private String nome;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }
---
+++ src/main/java/br/com/projetovendas/domain/repository/Clientes.java
@@ -0,0 +1,2 @@
+package br.com.projetovendas.domain.repository;public class Clientes {
+}
