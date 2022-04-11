d29bbb887e02905c1e6250079304211f1a8144bb
Carlos Henrique <c.heeenrique@gmail.com>
2022-04-10 22:37:18 -0300

    Refatoração de códigos para a utilização do EntityManager

--- src/main/java/br/com/projetovendas/SalesApplication.java
+++ src/main/java/br/com/projetovendas/SalesApplication.java
@@ -1,12 +1,53 @@
 package br.com.projetovendas;
 
+import br.com.projetovendas.domain.entity.Cliente;
+import br.com.projetovendas.domain.repository.Clientes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.context.annotation.Bean;
+
+import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SalesApplication {
+
+    @Bean
+    public CommandLineRunner init(@Autowired Clientes clientes){
+        return args -> {
+            System.out.println("Salvando clientes");
+            clientes.salvar(new Cliente("Dougllas"));
+            clientes.salvar(new Cliente("Outro Cliente"));
+
+            List<Cliente> todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Atualizando clientes");
+            todosClientes.forEach(c -> {
+                c.setNome(c.getNome() + " atualizado.");
+                clientes.atualizar(c);
+            });
+
+            todosClientes = clientes.obterTodos();
+            todosClientes.forEach(System.out::println);
+
+            System.out.println("Buscando clientes");
+            clientes.buscarPorNome("Cli").forEach(System.out::println);
+
+//            System.out.println("deletando clientes");
+//            clientes.obterTodos().forEach(c -> {
+//                clientes.deletar(c);
+//            });
+
+            todosClientes = clientes.obterTodos();
+            if(todosClientes.isEmpty()){
+                System.out.println("Nenhum cliente encontrado.");
+            }else{
+                todosClientes.forEach(System.out::println);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SalesApplication.class, args);
